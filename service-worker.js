b8ebc3776c2b887a64b2a537755700e880231cb8
bourgouinthomas <bourgouinthomas@gmail.com>
2026-01-21 21:47:06 +0100

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE="hub-personnel-v3";
-const FILES=["./","./index.html","./manifest.json"];
-self.addEventListener("install", e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener("fetch", e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
